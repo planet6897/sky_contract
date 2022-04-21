d4338c02b429b14fac7161977ff3d23f714c3e6b
Jaeook Ryu <jaeook.ryu@sky-technol.com>
2022-04-22 00:22:15 +0900

    SKPContext 제거

--- SKPERC20/SKPERC20.sol
+++ SKPERC20/SKPERC20.sol
@@ -4,11 +4,8 @@
 pragma solidity ^0.8.0;
 
 import "./ERC20.sol";
-import "./SKPContext.sol";
-import "./SafeMath.sol";
 
-contract SKPERC20 is SKPContext, ERC20 {
-    using SafeMath for uint256;
+contract SKPERC20 is ERC20 {
 
     constructor() ERC20("SKY PLAY", "SKP"){
         _mint(_msgSender(), 1e10 * (10 ** uint256(decimals())));
@@ -19,46 +16,5 @@ contract SKPERC20 is SKPContext, ERC20 {
      */
     function deposit() payable public {
         require(msg.value == 0, "Cannot deposit ether.");
-    }
-
-    function transfer(
-        address to,
-        uint256 amount
-    ) public override whenNotPaused whenPermitted(_msgSender()) returns (bool) {
-        super.transfer(to, amount);
-        return true;
-    }
-
-    function approve(
-        address spender,
-        uint256 amount
-    ) public override whenNotPaused whenPermitted(_msgSender()) returns (bool) {
-        super.approve(spender, amount);
-        return true;
-    }
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 amount
-    ) public override whenNotPaused whenPermitted(_msgSender()) returns (bool) {
-        super.transferFrom(from, to, amount);
-        return true;
-    }
-
-    function increaseAllowance(
-        address spender,
-        uint256 addedValue
-    ) public override whenNotPaused whenPermitted(_msgSender()) returns (bool) {
-        super.increaseAllowance(spender, addedValue);
-        return true;
-    }
-
-    function decreaseAllowance(
-        address spender,
-        uint256 subtractedValue
-    ) public override whenNotPaused whenPermitted(_msgSender()) returns (bool) {
-        super.decreaseAllowance(spender, subtractedValue);
-        return true;
     }
 }
